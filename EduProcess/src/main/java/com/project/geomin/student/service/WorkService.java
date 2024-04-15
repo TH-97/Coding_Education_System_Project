0ce199db45847bbb07b3fd1bba90ef6454f441db
Chambit <xornjseh0074@gmail.com>
2024-04-15 19:06:05 +0900

    6번째 커밋

--- EduProcess/src/main/java/com/project/geomin/student/service/WorkService.java
+++ EduProcess/src/main/java/com/project/geomin/student/service/WorkService.java
@@ -1,5 +1,7 @@
 package com.project.geomin.student.service;
 
+import com.project.geomin.command.GroupSearchVO;
+import com.project.geomin.command.GroupVO;
 import com.project.geomin.command.WorkVO;
 import com.project.geomin.util.Criteria;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +11,7 @@ import java.util.List;
 
 public interface WorkService {
     public ArrayList<WorkVO> getList(Criteria cri, String user_id);
+    ArrayList<GroupVO> selectGroup(GroupVO vo, GroupSearchVO gsvo);
     public int getTotal(Criteria cri, String user_id);
     public int regist(WorkVO vo);
     public WorkVO getDetail(int user_id);
