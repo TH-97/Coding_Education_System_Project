a2ab9444ae03b1e7d2cc2cf4aa3dd5687cb65fbe
ImHyukJin <ohj9575@gmail.com>
2024-04-18 17:14:56 +0900

    7차

--- EduProcess/src/main/java/com/project/geomin/controller/APIController.java
+++ EduProcess/src/main/java/com/project/geomin/controller/APIController.java
@@ -60,7 +60,7 @@ public class APIController {
         	
             // java 명령어를 사용하여 컴파일된 클래스 파일 실행
 //            Process runProcess = Runtime.getRuntime().exec("java -cp C:\\\\Users\\\\user\\\\Desktop Solution");
-            Process runProcess = Runtime.getRuntime().exec("/usr/bin/javac /home/ubuntu/"+user_id+"/"+hn+"/"+"Solution.java");
+            Process runProcess = Runtime.getRuntime().exec("/usr/bin/java /home/ubuntu/"+user_id+"/"+hn+"/"+"Solution.java");
             
             // 프로세스의 출력을 읽는 스레드 생성
             BufferedReader errorReader = new BufferedReader(new InputStreamReader(runProcess.getErrorStream()));
